e6e63e573a3ef355f396d5b70a6dc7ca53fce8b6
Klaudiusz Pastor <pastor.klo12@gmail.com>
2024-10-27 12:04:48 +0100

    added music block

--- blocks.h
+++ blocks.h
@@ -1,13 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",         "~/.dotfiles/scripts/spotify_status.sh",     5,        0},
 	{"",      "~/.dotfiles/scripts/weather_status.sh",   600,       13},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"",       "~/.dotfiles/scripts/system_status.sh",     5,        0},
 	{"",      "~/.dotfiles/scripts/volume_status.sh",    5,        10},
- 	{"", "$HOME/.dotfiles/scripts/battery_status.sh",   			 1,          0}, 
+ 	{"", "~/.dotfiles/scripts/battery_status.sh",   			 1,          0}, 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	{"",      "$HOME/.dotfiles/scripts/internet_status.sh",   10,       0},
+	{"",      "~/.dotfiles/scripts/internet_status.sh",   10,       0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
